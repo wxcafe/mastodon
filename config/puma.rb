28e521693f0d3d5fabe2823d59354d84fa79f987
Mastodon <mastodon@wxcafe.net>
2017-04-08 00:24:39 +0200

    local changes

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,11 @@
 threads_count = ENV.fetch('MAX_THREADS') { 5 }.to_i
 threads threads_count, threads_count
 
-port        ENV.fetch('PORT') { 3000 }
+#port        ENV.fetch('PORT') { 3000 }
 environment ENV.fetch('RAILS_ENV') { 'development' }
 workers     ENV.fetch('WEB_CONCURRENCY') { 2 }
+
+bind 'tcp://[::0]:3000'
 
 preload_app!
 
